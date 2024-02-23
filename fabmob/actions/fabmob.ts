3c3582b6a20bb5f56e8341957a0147318ec2120a
idillon <io.daza-dillon@savoirfairelinux.com>
2024-02-23 15:06:43 -0500

    Move loadTouristicPlaces to actions, rename it to fetchTouristicPlaces

--- fabmob/actions/fabmob.ts
+++ fabmob/actions/fabmob.ts
@@ -1,3 +1,23 @@
 import { createAction } from 'redux-actions'
 
+import { assembleBasePath } from '../../lib/actions/api'
+
 export const setTouristicPlaces = createAction('SET_TOURISTIC_PLACES')
+
+export const fetchTouristicPlaces = (): unknown => {
+  return async (dispatch: any, getState: any): Promise<void> => {
+    const state = getState()
+    const { config } = state.otp
+
+    const url = `${assembleBasePath(config)}/touristic-places`
+
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'GET'
+    })
+    const data = await response.json()
+    dispatch(setTouristicPlaces(data.touristicPlaces))
+  }
+}
--- fabmob/components/map/touristic-places-overlay.tsx
+++ fabmob/components/map/touristic-places-overlay.tsx
@@ -2,40 +2,21 @@ import { connect } from 'react-redux'
 import { MarkerWithPopup } from '@opentripplanner/base-map'
 import React, { ReactElement, useState } from 'react'
 
-import { assembleBasePath } from '../../../lib/actions/api'
-import { setTouristicPlaces } from '../../actions/fabmob'
+import { fetchTouristicPlaces } from '../../actions/fabmob'
 import { TouristicPlace } from '../../reducers/create-fabmob-reducer'
 
-export const loadTouristicPlaces = (): unknown => {
-  return async (dispatch: any, getState: any): Promise<void> => {
-    const state = getState()
-    const { config } = state.otp
-
-    const url = `${assembleBasePath(config)}/touristic-places`
-
-    const response = await fetch(url, {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'GET'
-    })
-    const data = await response.json()
-    dispatch(setTouristicPlaces(data.touristicPlaces))
-  }
-}
-
 type TouristicPlacesOverlayProps = {
-  loadTouristicPlaces: () => void
+  fetchTouristicPlaces: () => void
   touristicPlaces: TouristicPlace[]
 }
 
 const TouristicPlacesOverlay = ({
-  loadTouristicPlaces,
+  fetchTouristicPlaces,
   touristicPlaces
 }: TouristicPlacesOverlayProps): ReactElement => {
   const [isFirstLoad, setIsFirstLoad] = useState(true)
   if (isFirstLoad) {
-    loadTouristicPlaces()
+    fetchTouristicPlaces()
     setIsFirstLoad(false)
   }
   return (
@@ -69,7 +50,7 @@ const mapStateToProps = (state: any) => {
 }
 
 const mapDispatchToProps = {
-  loadTouristicPlaces
+  fetchTouristicPlaces
 }
 
 export default connect(
