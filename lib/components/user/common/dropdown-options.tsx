6b8e1775ea625b318892f2ef6571a994eedabf19
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2024-12-20 13:21:15 -0500

    refactor(DropdownOptions): Delete unused disabled prop

--- lib/components/user/common/dropdown-options.tsx
+++ lib/components/user/common/dropdown-options.tsx
@@ -7,7 +7,6 @@ interface SelectProps {
   Control?: ComponentType
   children: ReactNode
   defaultValue?: string | number | boolean
-  disabled?: boolean
   label?: ReactNode
   name: string
   onChange?: ChangeEventHandler
@@ -20,7 +19,6 @@ export const Select = ({
   Control = FormControl,
   children,
   defaultValue,
-  disabled,
   label,
   name,
   onChange
@@ -29,7 +27,6 @@ export const Select = ({
     as: Control,
     componentClass: 'select',
     defaultValue,
-    disabled,
     id: name,
     name,
     onChange
