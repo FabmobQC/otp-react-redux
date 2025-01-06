f2af8414da633b2dadea7c1b53dabd2fff44ca7c
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2025-01-06 07:38:44 -0500

    refactor(withSuspense): Use generics for props

--- lib/components/util/with-suspense.tsx
+++ lib/components/util/with-suspense.tsx
@@ -3,10 +3,10 @@ import React, { ComponentType, Suspense } from 'react'
 /**
  * Wraps a component with suspense, assuming the wrapped component is lazily loaded.
  */
-export default function withSuspense(
-  WrappedComponent: ComponentType
-): ComponentType {
-  const suspensedComponent = (props: any): JSX.Element => (
+export default function withSuspense<T>(
+  WrappedComponent: ComponentType<T>
+): ComponentType<T> {
+  const suspensedComponent = (props: T): JSX.Element => (
     <Suspense fallback={<span />}>
       <WrappedComponent {...props} />
     </Suspense>
