fc073abf903396f916efa55c3ac340eb595a55be
Io A. Daza-Dillon <io@dazadillon.io>
2026-08-24 17:11:42 -0400

    fabmob: Have AllRoutesViewerOverlay center around all routes

--- fabmob/components/map/all-routes-viewer-overlay.tsx
+++ fabmob/components/map/all-routes-viewer-overlay.tsx
@@ -1,5 +1,9 @@
 import { connect } from 'react-redux'
-import React from 'react'
+import { LngLatBounds } from 'maplibre-gl'
+import { useMap } from 'react-map-gl/maplibre'
+import { util } from '@opentripplanner/base-map'
+import polyline from '@mapbox/polyline'
+import React, { useEffect } from 'react'
 import RouteViewerOverlay from '@opentripplanner/route-viewer-overlay'
 
 const getRouteData = (state: any) => {
@@ -40,9 +44,76 @@ interface AllRoutesViewerOverlayProps {
   routesData: any[]
 }
 
+const extendBoundsWithCoordinates = (
+  bounds: LngLatBounds | undefined,
+  coordinates: any
+): LngLatBounds | undefined => {
+  if (typeof coordinates?.[0] === 'number') {
+    const [lng, lat] = coordinates
+    return bounds
+      ? bounds.extend([lng, lat])
+      : new LngLatBounds([lng, lat], [lng, lat])
+  }
+  return (coordinates || []).reduce(extendBoundsWithCoordinates, bounds)
+}
+
+const extendBoundsWithGeometry = (
+  bounds: LngLatBounds | undefined,
+  geometry: any
+): LngLatBounds | undefined => {
+  if (!geometry) return bounds
+  if (geometry.type === 'GeometryCollection') {
+    return (geometry.geometries || []).reduce(extendBoundsWithGeometry, bounds)
+  }
+  return extendBoundsWithCoordinates(bounds, geometry.coordinates)
+}
+
+const extendBoundsWithRoute = (
+  bounds: LngLatBounds | undefined,
+  routeData: any
+): LngLatBounds | undefined => {
+  const patterns: any[] = Object.values(routeData?.patterns || {})
+  let newBounds = bounds
+  patterns.forEach((ptn) => {
+    if (ptn?.geometry?.points) {
+      polyline.decode(ptn.geometry.points).forEach(([lat, lng]) => {
+        newBounds = newBounds
+          ? newBounds.extend([lng, lat])
+          : new LngLatBounds([lng, lat], [lng, lat])
+      })
+    }
+    ptn?.stops?.forEach((stop: any) => {
+      const geoJson = stop?.geometries?.geoJson
+      if (geoJson) {
+        newBounds = extendBoundsWithGeometry(newBounds, geoJson)
+      }
+    })
+  })
+  return newBounds
+}
+
 const AllRoutesViewerOverlay = (props: AllRoutesViewerOverlayProps) => {
   const { routesData } = props
-  const shouldNotCenterMap = routesData.length > 1
+  const { current } = useMap()
+  const isMultiRoute = routesData.length > 1
+
+  useEffect(() => {
+    if (!isMultiRoute || !current) {
+      return undefined
+    }
+
+    const bounds = routesData.reduce(extendBoundsWithRoute, undefined)
+
+    let timeout: ReturnType<typeof setTimeout> | undefined
+    if (bounds) {
+      const fitBounds = () => util.fitMapBounds(current, bounds)
+      fitBounds()
+      timeout = setTimeout(fitBounds, 250)
+    }
+
+    return () => clearTimeout(timeout)
+  }, [routesData, isMultiRoute, current])
+
   return routesData.map((routeData) => {
     return (
       <RouteViewerOverlay
@@ -51,7 +122,7 @@ const AllRoutesViewerOverlay = (props: AllRoutesViewerOverlayProps) => {
         // eslint-disable-next-line @typescript-eslint/no-empty-function
         mapCenterCallback={() => {}}
         routeData={routeData}
-        shouldNotCenterMap={shouldNotCenterMap}
+        shouldNotCenterMap={isMultiRoute}
       />
     )
   })
